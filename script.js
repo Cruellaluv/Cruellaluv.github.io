cb1f0b8ee58cc0a8aaa1f0d17c2a31f8aedf685f
YorgelisCastill <88332074+Cruellaluv@users.noreply.github.com>
2021-08-05 23:17:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function hoverGrow(){
+  var grow = document.getElementById("grow")
+  grow.style.fontSize="40px"
+  grow.style.color="lightblue"
+
+}
+    
+
+function changeBackGround(){
+  document.body.style.backgroundColor = "lightblue"
+}
+
+
+
+
+function pressed() {
+  var press = document.getElementById("press")
+  press.style.color="blue"
+}
+
